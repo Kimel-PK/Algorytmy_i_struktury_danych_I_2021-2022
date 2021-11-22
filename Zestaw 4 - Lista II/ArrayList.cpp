285b1eb18a9d15448c697359144d46abc0297896
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2021-11-22 09:35:00 +0100

    Usunięcie komentarzy z prób debugowania

--- Zestaw 4 - Lista II/ArrayList.cpp
+++ Zestaw 4 - Lista II/ArrayList.cpp
@@ -8,36 +8,6 @@ using namespace std;
 
 int main (int argc, char *argv[]) {
 	
-	/*
-	
-	ArrayList<ROZMIAR> lista = ArrayList<ROZMIAR>();
-	
-	lista.push_front(69);
-	lista.push_front(1);
-	lista.push_front(2);
-	lista.push_front(69);
-	lista.push_front(69);
-	lista.push_front(3);
-	lista.push_front(4);
-	lista.push_front(5);
-	lista.push_front(69);
-	lista.push_front(6);
-	lista.push_front(7);
-	lista.push_front(69);
-	lista.push_front(69);
-	lista.push_front(69);
-	lista.push_front(8);
-	lista.push_front(9);
-	lista.push_front(69);
-	lista.push_front(10);
-	lista.push_front(69);
-	
-	cout << "remove " << lista.remove(69) << endl;
-	
-	lista.WypiszListe();
-	
-	*/
-	
 	char wejscie_char;
 	int wejscie_int;
 	int operacje;
